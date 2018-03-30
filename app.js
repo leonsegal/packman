e82b8edae3672a641570d0ed848dc1b511607000
leon segal <leonsegal1@gmail.com>
2018-03-30 01:39:35 +0100

    get formula working

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const 
-	order = [555, 300, 400],
-	items = [{vol: 3, wgt: 1, count: order[0]}, {vol: 2, wgt: 2, count: order[1]}, {vol: 1, wgt: 3, count: order[2]}],
-	boxes = [{maxVol: 30, maxWgt: 15}, {maxVol: 20, maxWgt: 10}, {maxVol: 10, maxWgt: 5}],
-	boxMaxes = []
+	order = [767826826594, 3000, 4000],
+	items = [{vol: 6, wgt: 1, count: order[0]}, {vol: 2, wgt: 2, count: order[1]}, {vol: 1, wgt: 3, count: order[2]}],
+	boxes = [{maxVol: 40, maxWgt: 15}, {maxVol: 20, maxWgt: 10}, {maxVol: 5, maxWgt: 5}],
+	boxMaxes = [];
 	boxNumber = 0;
 
 function getBoxCount(itemVol, boxVol) {
-	const 
-		remainder =  itemVol % boxVol,
-		numBoxes = Math.round(itemVol / boxVol);
+	const numBoxes = Math.round(itemVol / boxVol);
 
-		boxMaxes.push(numBoxes);
+	boxMaxes.push(numBoxes);
 
 	boxNumber += 1;
 
-	if (boxNumber < numBoxes) {
-		getBoxCount(remainder, boxes[boxNumber].maxVol);
+	if (itemVol / boxVol > numBoxes) {
+		getBoxCount(itemVol % boxVol, boxes[boxNumber].maxVol);
 	}
 }
 
