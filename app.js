3703387a5d375297fe0f9edeae6a71658b198393
leon segal <leonsegal1@gmail.com>
2018-03-31 00:20:19 +0100

    add weight factor and app speed

--- app.js
+++ app.js
@@ -1,39 +1,27 @@
+console.time('app time');
 const
-	orders = [26, 10, 10],
+	_ = require('lodash');
+	orders = [777, 10, 10],
 	items = [
-		{
-			vol: 10,
-			wgt: 1,
-			count: orders[0],
-		},
-		{
-			vol: 32,
-			wgt: 2,
-			count: orders[1],
-		},
-		{
-			vol: 11,
-			wgt: 3,
-			count: orders[2],
-		},],
-	boxes = [
-		{
-			maxVol: 100,
-			maxWgt: 15,
-		},
-		{
-			maxVol: 60,
-			maxWgt: 10,
-		},
-		{
-			maxVol: 25,
-			maxWgt: 5,
-		},
+		{vol: 20, wgt: 1, count: orders[0]},
+		{vol: 32, wgt: 2, count: orders[1]},
+		{vol: 11, wgt: 3, count: orders[2]}
 	],
-	// [ 10, 6, 2 ] [ 3, 1, 0 ] [ 9, 5, 2 ]
-	itemsBoxes = items.map(item => boxes.map(box => Math.floor(box.maxVol / item.vol)));
+	boxes = [
+		{maxVol: 74, maxWgt: 15},
+		{maxVol: 60, maxWgt: 10},
+		{maxVol: 25, maxWgt: 5}
+	],
+	itemsBoxesVol = _.flatten(items.map(item => boxes.map(box => Math.floor(box.maxVol / item.vol)))),
+	itemsBoxesWgt = _.flatten(items.map(item => boxes.map(box => Math.floor(box.maxWgt / item.wgt)))),
+	zipVolWgt = _.zip(itemsBoxesVol, itemsBoxesWgt),
+	minCapacities = [];
 
-const getBoxCount = (items, boxList = [], step = 0, boxCapacityList = itemsBoxes[0]) => {
+_.forEach(zipVolWgt,item => minCapacities.push(item[0] < item[1] ? item[0] : item[1]));
+minCapacities = _.chunk(minCapacities,3);
+console.log(minCapacities);
+
+const getBoxCount = (items, boxList = [], step = 0, boxCapacityList = itemsBoxesVol[0]) => {
 	const
 		boxCount = Math.floor(items / boxCapacityList[step]),
 		remainder = items % boxCapacityList[step];
@@ -49,6 +37,5 @@ const getBoxCount = (items, boxList = [], step = 0, boxCapacityList = itemsBoxes
 	return boxList;
 };
 
-finalList = getBoxCount(orders[0]);
-
-console.log(finalList);
+// console.log(getBoxCount(orders[0]));
+console.timeEnd('app time');
