3ecf223ea84be629444c82834464bc050b53ffea
leon segal <leonsegal1@gmail.com>
2018-03-31 00:29:23 +0100

    code tidy

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 console.time('app time');
 const
 	_ = require('lodash');
-	orders = [777, 10, 10],
+	orders = [777, 10, 10, 3],
 	items = [
-		{vol: 20, wgt: 1, count: orders[0]},
+		{vol: 10, wgt: 3, count: orders[0]},
 		{vol: 32, wgt: 2, count: orders[1]},
-		{vol: 11, wgt: 3, count: orders[2]}
+		{vol: 11, wgt: 3, count: orders[2]},
+		{vol: 44, wgt: 3, count: orders[3]},
 	],
 	boxes = [
 		{maxVol: 74, maxWgt: 15},
 		{maxVol: 60, maxWgt: 10},
-		{maxVol: 25, maxWgt: 5}
+		{maxVol: 25, maxWgt: 5},
 	],
 	itemsBoxesVol = _.flatten(items.map(item => boxes.map(box => Math.floor(box.maxVol / item.vol)))),
 	itemsBoxesWgt = _.flatten(items.map(item => boxes.map(box => Math.floor(box.maxWgt / item.wgt)))),
@@ -19,9 +20,8 @@ const
 
 _.forEach(zipVolWgt,item => minCapacities.push(item[0] < item[1] ? item[0] : item[1]));
 minCapacities = _.chunk(minCapacities,3);
-console.log(minCapacities);
 
-const getBoxCount = (items, boxList = [], step = 0, boxCapacityList = itemsBoxesVol[0]) => {
+const getBoxCount = (items, boxList = [], step = 0, boxCapacityList = minCapacities[0]) => {
 	const
 		boxCount = Math.floor(items / boxCapacityList[step]),
 		remainder = items % boxCapacityList[step];
@@ -37,5 +37,5 @@ const getBoxCount = (items, boxList = [], step = 0, boxCapacityList = itemsBoxes
 	return boxList;
 };
 
-// console.log(getBoxCount(orders[0]));
+console.log(getBoxCount(orders[0]));
 console.timeEnd('app time');
