cac9b981df8f919f781130f1b0d6d13611d88ab1
leon segal <leonsegal1@gmail.com>
2018-03-30 15:13:48 +0100

    add ignore file and code tidy

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const 
+const
 	order = [26, 10, 10],
 	items = [
-		{vol: 17, wgt: 1, count: order[0]}, 
-		{vol: 32, wgt: 2, count: order[1]}, 
-		{vol: 11, wgt: 3, count: order[2]}
+		{vol: 17, wgt: 1, count: order[0]},
+		{vol: 32, wgt: 2, count: order[1]},
+		{vol: 11, wgt: 3, count: order[2]},
 	],
 	boxes = [
-		{maxVol: 10, maxWgt: 15}, 
-		{maxVol: 9, maxWgt: 10}, 
-		{maxVol: 8, maxWgt: 5}
+		{maxVol: 10, maxWgt: 15},
+		{maxVol: 9, maxWgt: 10},
+		{maxVol: 8, maxWgt: 5},
 	],
 	bigList = [];
 
@@ -28,7 +28,7 @@ function getBoxCountList(itemsVol, boxVol, boxMaxList = [], boxNumber = 0) {
 }
 
 items.forEach(item => {
-	bigList.push(boxList = getBoxCountList(item.vol * item.count, boxes[0].maxVol));
+	bigList.push(getBoxCountList(item.vol * item.count, boxes[0].maxVol));
 });
 
 console.log(bigList);
