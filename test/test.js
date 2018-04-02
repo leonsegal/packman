f87b2c18543e3e4e57ac8ade3ef663c5f15cb0d1
leon segal <leonsegal1@gmail.com>
2018-04-02 01:15:22 +0100

    add some starting tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,23 @@
 // const app = require('../app');
 const app = require('../app');
 
-QUnit.test( "boxcount present", assert => {
+QUnit.test( "one big box", assert => {
 	assert.deepEqual(
-		[ [ 'box 1', 1 ] ],
 		app.getBoxCount([10], [10, 5, 2]),
-		"boxcount is present!");
+		[ [ 'box 1', 1 ] ],
+		"should return one big box");
+});
+
+QUnit.test( "one big box, one medium box", assert => {
+	assert.deepEqual(
+		app.getBoxCount([15], [10, 5, 2]),
+		[ [ 'box 1', 1 ], [ 'box 2', 1 ] ],
+		"should return one big box, one medium box");
+});
+
+QUnit.test( "one big box, one medium box, one small box", assert => {
+	assert.deepEqual(
+		app.getBoxCount([17], [10, 5, 2]),
+		[ [ 'box 1', 1 ], [ 'box 2', 1 ], [ 'box 3', 1 ] ],
+		"should return one big box, one medium box");
 });
