ed5b7411bd722604796f5a4bb31bfc6a13108179
leon segal <leonsegal1@gmail.com>
2018-04-02 13:12:08 +0100

    code tidy

--- test/test.js
+++ test/test.js
@@ -1,29 +1,25 @@
 const app = require('../app');
 
-QUnit.test( "one big box", assert => {
-	assert.deepEqual(
+QUnit.test( "one big box", assert => assert.deepEqual(
 		app.getBoxCount([10], [10, 5, 2]),
 		[ [ 'box 1', 1 ] ],
-		"should return one big box");
-});
+		"should return one big box")
+);
 
-QUnit.test( "one big box, one medium box", assert => {
-	assert.deepEqual(
+QUnit.test( "one big box, one medium box", assert => assert.deepEqual(
 		app.getBoxCount([15], [10, 5, 2]),
 		[ [ 'box 1', 1 ], [ 'box 2', 1 ] ],
-		"should return one big box, one medium box");
-});
+		"should return one big box, one medium box")
+);
 
-QUnit.test( "one big box, one medium box, one small box", assert => {
-	assert.deepEqual(
+QUnit.test( "one big box, one medium box, one small box", assert => assert.deepEqual(
 		app.getBoxCount([17], [10, 5, 2]),
 		[ [ 'box 1', 1 ], [ 'box 2', 1 ], [ 'box 3', 1 ] ],
-		"should return one big box, one medium box, one small box");
-});
+		"should return one big box, one medium box, one small box")
+);
 
-QUnit.test( "overflow", assert => {
-	assert.deepEqual(
+QUnit.test( "overflow", assert => assert.deepEqual(
 		app.getBoxCount([18], [10, 5, 2]),
 		[ [ 'box 1', 1 ], [ 'box 2', 1 ], [ 'box 3', 2 ] ],
-		"should return one big box, one medium box, two small boxes");
-});
+		"should return one big box, one medium box, two small boxes")
+);
